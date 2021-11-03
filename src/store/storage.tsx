e602952a031f4b9971fafc8f9b9e6ffc3bca3b57
Denys <denysexample5@gmail.com>
2021-11-03 23:35:55 +0200

    fix

---
+++ src/store/storage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+
+export const storage = {
+    setItem: (name:  string, item: any) => {
+      localStorage.setItem(name, JSON.stringify(item))
+    },
+    getItem: (name: string) => {
+      const item = localStorage.getItem(name);
+
+      if(item) {
+          return JSON.parse(item);
+      }
+    }
+}
+
+export default storage;
