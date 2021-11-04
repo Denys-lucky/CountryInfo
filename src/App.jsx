81832549f4c40c26c695c619729d6381475d15d4
Denys <denysexample5@gmail.com>
2021-11-04 15:04:19 +0200

    fixing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { NonReactStatics } from 'hoist-non-react-statics';
 
 import { Header } from './components/Header';
 import { Main } from './components/Main';
